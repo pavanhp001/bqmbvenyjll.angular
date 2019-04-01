50833cd5f1b27b51eaa4f3031a685f17661210ec
pavanhp001 <pavan.hp001@gmail.com>
2019-04-01 10:00:56 +0530

    app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { CoursesComponent } from './courses/courses.component';
+import { SummeryPipe } from './summery.pipe';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    CoursesComponent,
+    SummeryPipe,
+    FavoriteComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,//for directives
     FormsModule
   ],
-  providers: [
-    // no need to place any providers due to the `providedIn` flag...
-  ],
-  bootstrap: [ AppComponent ]
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
